dfad57bad6e096907806cb4b2a4d7fb0afca14d5
Pedro Velasco Santana <120191699+LeviDooku@users.noreply.github.com>
2023-03-12 18:57:59 +0100

    Modified Show_Matrix function and added new one

---
+++ Basic_data_base/include/User_Pssw_Matrix_Class.h
@@ -0,0 +1,56 @@
+#ifndef USER_PSSW_MATRIX_CLASS
+#define USER_PSSW_MATRIX_CLASS
+
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+class User_Pssw_Matrix{
+
+private:
+	static const int MAX_ROWS = 100;
+	static const int MAX_COL = 2;
+	static const int USER_NAME_MAX_CAR = 15;
+	static const int USER_NAME_MIN_CAR = 3;
+	static const int PSSW_MIN_CAR = 8;
+	int num_rows;
+	string matrix[MAX_ROWS][MAX_COL];
+
+public: 
+
+	User_Pssw_Matrix();
+
+	int Get_Rows();
+
+	int Find_User_Index(string user_name);
+
+	string Get_User_Name(string user_name);
+
+	string Get_User_Password(string user_name);
+
+	string Get_User_Info(string user_name);
+
+	void Set_User_Name(string new_user_name, string old_user_name);
+
+	void Set_User_Password(string new_password, string old_password, string user_name);
+
+	void Delete_User(string user_name);
+
+	bool User_Name_Comprobation(string user_name);
+
+	bool Char_Comprobation_un(string user_name);
+
+	bool Char_Comprobation_pssw(string password);
+
+	void Add_User(string user_name, string password);
+
+	void Show_Matrix();
+
+	void Save_To_File();
+
+	//void Load_From_File();
+
+};
+
+#endif
---
+++ Basic_data_base/src/Basic_Data_Base.cpp
@@ -0,0 +1,180 @@
+#include "User_Pssw_Matrix_Class.h"
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+//Function for adding new users.
+void Add_New_User(User_Pssw_Matrix& data_base){
+
+	string user_name;
+	string password;
+
+	cout << "Introduzca el nombre de usuario: " << endl;
+	cin >> user_name;
+
+	//Checking if the user name meets the requirements (methods described in User_Class_Matrix_Class.cpp)
+	while(data_base.Char_Comprobation_un(user_name) || data_base.User_Name_Comprobation(user_name)){
+
+		if(data_base.Char_Comprobation_un(user_name)){
+			cout << "El nombre de usuario no puede superar los 15 caracteres ni tener menos de 3, pruebe nuevo: " << endl;
+			cin >> user_name;
+		}
+		else{
+			cout << "El nombre de usuario escogido está en uso, pruebe de nuevo: " << endl;
+			cin >> user_name;
+		}
+
+	}
+
+	cout << "Introduzca ahora la contraseña: " << endl;
+	cin >> password;
+
+	//Same for the password
+	while(data_base.Char_Comprobation_pssw(password)){
+		cout << "La contraseña debe contener al menos 8 caracteres, pruebe de nuevo: " << endl;
+		cin >> password;
+	}
+
+	data_base.Add_User(user_name, password);
+
+	cout << endl << "¡Usuario registrado con éxito!" << endl;
+
+}
+
+//Function to Log in and modify things about the user
+void Log_In(User_Pssw_Matrix& data_base){
+	
+	string user_name;
+	string password;
+	char option;
+
+	cout << "Introduzca el nombre de usuario: " << endl;
+	cin >> user_name;
+
+	//Checking if the credentials are correct
+	while(data_base.User_Name_Comprobation(user_name) == false){
+		cout << "Usuario no encontado, pruebe de nuevo: " << endl;
+		cin >> user_name;
+	}
+
+	cout << "Introduzca la contraseña: " << endl;
+	cin >> password;
+
+	while(data_base.Get_User_Password(user_name) != password){
+		cout << "Contraseña incorrecta, pruebe de nuevo: " << endl;
+		cin >> password;
+	}
+
+	cout << endl << "¡Sesión iniciada con éxito!, bienvenid@: " << user_name << endl;
+
+	//Opening a sub-menu with an infinite while structure
+	while(true){
+		cout << endl;
+		cout << "a) Modificar nombre de usuario" << endl;	//Modify user name
+		cout << "b) Modificar contraseña" << endl;	//Modify password
+		cout << "c) Borrar usuario" << endl;	//Delete user
+		cout << "d) Cerrar sesión" << endl;	//Go to the main menu
+		cout << "Seleccione una acción: ";
+		cin >> option;
+
+		if(option == 'a'){
+			string new_user_name;
+
+			cout << "Introduzca un nuevo nombre de usuario " << "(nombre actual " << user_name << "): " << endl;
+			cin >> new_user_name; 
+
+			//Checking if the new user name meet the requirements
+			while(data_base.User_Name_Comprobation(new_user_name) || data_base.Char_Comprobation_un(new_user_name)){
+				char option_name;
+
+				cout << "El nombre de usuario (" << new_user_name << ") ya ha sido seleccionado o no cumple los requisitos" << endl;
+				cout << "Pruebe de nuevo: " << endl;
+				cin >> new_user_name;		
+			}
+
+			data_base.Set_User_Name(new_user_name, user_name);
+
+			cout << endl << "¡Cambio realizado con éxito!" << endl;
+		}
+		else if(option == 'b'){
+			string new_password;
+
+			cout << "Introduzca una nueva contraseña: " << endl;
+			cin >> new_password;
+
+			while(data_base.Char_Comprobation_pssw(new_password) || data_base.Get_User_Password(user_name) == new_password){
+				char option_pssw;
+
+				cout << "La contraseña no puede ser más corta de 8 caracteres ni ser igual a la anterior" << endl;
+				cout << "Pruebe de nuevo: " << endl;
+				cin >> new_password;
+			}
+
+			data_base.Set_User_Password(new_password, password, user_name);
+
+			cout << endl << "¡Cambio realizado con éxito!" << endl;
+
+		}
+		else if(option == 'c'){
+			char seguro;
+
+			//Confirmation message
+			cout << "¿Quiere borrar su usuario? (Y/N): ";
+			cin >> seguro;
+
+			if(seguro != 'Y'){
+				return;
+			}
+			else
+				data_base.Delete_User(user_name);
+				cout << "Usuario borrado";
+		}
+		else if(option == 'd'){
+			break;
+		}
+	}
+}
+
+
+int main(){
+
+	User_Pssw_Matrix data_base;
+	char option;
+
+	//Save_To_File(data_base);
+	
+	//Opening a menu with an infinite while structure 
+	while(true){
+
+		cout << endl;
+		cout << "a) Registrar usuario" << endl;
+		cout << "b) Iniciar sesión" << endl;
+		cout << "q) Salir del menú" << endl;
+		cout << "¿Que desea hacer?: ";
+		cin >> option;
+
+		if(option == 'a'){
+			Add_New_User(data_base);
+		}
+		else if(option == 'b'){			
+			Log_In(data_base);
+		}
+		else if(option == 'q'){
+			break;
+		}
+
+		cout << endl << endl; 
+
+		//I show the matrix in the console to make sure all running ok
+		cout << "************************" << endl;
+		data_base.Show_Matrix();
+		cout << "************************" << endl;
+		
+		cout << endl << endl;
+
+		data_base.Save_To_File();
+	}
+	
+	return 0;
+}
